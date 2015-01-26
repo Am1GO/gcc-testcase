42b62ed3f7d895719d27604deb9bdf2906946740
Vladimir Stackov <amigo.elite@gmail.com>
2015-01-26 03:21:12 +0300

    CXX

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash -x
 rm -f testcase.pb* testcase
 protoc testcase.proto --cpp_out=./
-
-case "$1" in
-  gcc)
-    g++ -g -lprotobuf -lpthread -o testcase *cc
-    ;;
-  clang)
-    clang++ -g -lprotobuf -lpthread -o testcase *cc
-    ;;
-  *)
-    echo "Usage: $0 <gcc|clang>"
-    ;;
-esac
+[[ -z $CXX ]] && CXX=g++
+$CXX -g -lprotobuf -lpthread -o testcase *cc
